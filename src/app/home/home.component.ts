7351c3aadf66f062cee6f12a2e1127321fcb522d
daicathay76 <danghung76@yahoo.com>
2023-11-04 08:48:52 +0700

    xong

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  public name: string = 'Hưng'
+  public name: string = 'Hưng Nguyễn'
   constructor() { }
   ngOnInit(): void { }
 }
