11a2f641b86f67b1963234f8f357584c91b4205e
daicathay76 <danghung76@yahoo.com>
2023-11-04 08:59:10 +0700

    done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   public name: string = 'Hưng Nguyễn'
+  public age: number = 25;
   constructor() { }
   ngOnInit(): void { }
 }
