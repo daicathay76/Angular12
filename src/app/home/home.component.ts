c8287eb60a23ffbc250d193be0bf864b5029311e
daicathay76 <danghung76@yahoo.com>
2023-11-04 11:53:22 +0700

    done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,15 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   public name: string = 'Hưng Nguyễn'
   public age: number = 25;
+  public traicay: string[] = ['Táo', 'Nho'];
+  public traicay2 = [
+    { ten: 'Táo', gia: 12, hagia: true },
+    { ten: 'Nho', gia: -3, hagia: false },
+    { ten: 'Cam', gia: 15, hagia: true }
+  ];
   constructor() { }
-  ngOnInit(): void { }
+  public ngOnInit(): void {
+    console.log('Trái cây:', this.traicay);
+    console.log('Trái cây 2:', this.traicay2);
+  }
 }
