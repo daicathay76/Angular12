c1a1cc34e8007d2d1ddb1a83f49488a7f0f45cdf
daicathay76 <danghung76@yahoo.com>
2023-11-04 17:13:29 +0700

    chỉnh table

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,11 +9,7 @@ export class HomeComponent {
   public name: string = 'Hưng Nguyễn'
   public age: number = 25;
   public traicay: string[] = ['Táo', 'Nho'];
-  public traicay2 = [
-    { ten: 'Táo', gia: 12, hagia: true },
-    { ten: 'Nho', gia: -3, hagia: false },
-    { ten: 'Cam', gia: 15, hagia: true }
-  ];
+  public traicay2 = [    { ten: 'Táo', gia: 12, hagia: true },   { ten: 'Nho', gia: -3, hagia: false }, { ten: 'Cam', gia: 15, hagia: true } ];
   constructor() { }
   public ngOnInit(): void {
     console.log('Trái cây:', this.traicay);
