de56690a03de2bb79679a375db10caa1979a4065
daicathay76 <danghung76@yahoo.com>
2023-11-04 08:30:22 +0700

    xong

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  public name: string = 'Hưng'
+  constructor() { }
+  ngOnInit(): void { }
 }
