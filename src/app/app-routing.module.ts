759c805642f52a7886e8c6db1e74164b44511d6a
daicathay76 <danghung76@yahoo.com>
2023-11-05 16:22:21 +0700

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'About', component: AboutComponent },
+  { path: 'Home', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent }, // nhấn ctrl + . để import cho đúng component
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { OnSalesPipe } from './on-sales.pipe';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    OnSalesPipe
+    OnSalesPipe,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
